2261c870131f3aeba83b76c7e5659cacb029d478
quantinium03 <vaibhavvx331@gmail.com>
2025-03-15 02:34:46 +0530

    dark mode and light mode

--- src/colors.ts
+++ src/colors.ts
@@ -4,21 +4,19 @@ import path from "path";
 import { readFile } from "fs/promises";
 
 export const generateCSSFile = async () => {
-    let css = "/*DO NOT CHANGE ANYTHING IN THIS FILE. THIS FILE IS PREGENERATED AND WILL BE OVERWRITTEN.*/\n:root {\n";
+    let css = "/*DO NOT CHANGE ANYTHING IN THIS FILE. THIS FILE IS PREGENERATED AND WILL BE OVERWRITTEN.*/\n.light {\n";
     for (const [name, value] of Object.entries(grimoire.theme.colors.lightMode)) {
         css += `    --${name}: ${value};\n`;
     }
     css += "}\n\n";
-    css += "@media (prefers-color-scheme: dark) {\n";
-    css += "    :root {\n";
+    css += ".dark{\n";
     for (const [name, value] of Object.entries(grimoire.theme.colors.darkMode)) {
         css += `    --${name}: ${value};\n`;
     }
     css += "}\n";
-    css += "}";
 
     const precss: string = await readFile("./src/templates/assets/styles.css", "utf-8")
-    const newcss = precss.split('\n').slice(26).join('\n')
+    const newcss = precss.split('\n').slice(24).join('\n')
     css += newcss;
     writeFile(path.join("src", "templates/assets/styles.css.bak"), precss);
     writeFile(path.join("src", "templates/assets/styles.css"), css);
