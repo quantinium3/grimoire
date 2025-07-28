ebebf7f75338fac796c424094d2e5aa2acbb08d2
quantinium03 <quant@quantinium.dev>
2025-07-28 15:55:59 +0530

    init command added

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -1,45 +1,71 @@
-use anyhow::{bail, Context, Result};
-use tokio::fs::{create_dir_all, try_exists};
+use anyhow::{Context, Result};
+use colored::Colorize;
+use rust_embed::RustEmbed;
+use std::path::Path;
+use tokio::fs::{self, create_dir_all};
+
+#[derive(RustEmbed)]
+#[folder = "static/"]
+struct StaticAssets;
 
 pub async fn init_project(name: &str) -> Result<()> {
-    // initialize project
-    // @name - name of the project
-    // create all dirs -> create all files -> put content in those files
-    // file :- content/blog, content/pages, templates/blog.hbs, templates/index.hbs,
-    // templates/page.hbs
-    //
-    // println -> cd ${name} 
-    // println -> grimoire build
-    println!("initializing project: {}", name);
+    let path = Path::new(name);
 
-    ensure_dir(name).await?;
-    initialize_project(name).await?;
-    
-    Ok(())
-}
+    create_dir_all(path)
+        .await
+        .with_context(|| format!("Failed to create directory: {}", name))?;
 
-async fn initialize_project(name: &str) -> Result<()> {
     let dirs = [
+        "templates",
         "content/blog",
         "content/pages",
-        "templates",
-        "static/css",
+        "static/js",
         "static/images",
-        "static/js"
+        "static/styles",
     ];
 
     for dir in dirs {
-        let full_path = format!("{}/{}", name, dir);
-        create_dir_all(&full_path).await.with_context(|| format!("Failed to create directory: '{}'", full_path))?;
+        let dir_path = path.join(dir);
+        create_dir_all(&dir_path)
+            .await
+            .with_context(|| format!("Failed to create directory: {}", dir_path.display()))?;
     }
+
+    let template_files = [
+        ("templates/index.hbs"),
+        ("templates/blog.hbs"),
+        ("templates/page.hbs"),
+    ];
+
+    for file in template_files {
+        write_embedded_file(path, file)
+            .await
+            .with_context(|| format!("Failed to write file: {}", file))?;
+    }
+
+    println!("Initialized new project: {}", name);
+    println!("{}", "run:".cyan());
+    println!("    cd {}", name.cyan());
+    println!("    {}", "grimoire build".cyan());
 
     Ok(())
 }
-async fn ensure_dir(name: &str) -> Result<()> {
-    if try_exists(name).await? {
-        bail!("'{}' already exist", name);
+
+async fn write_embedded_file(project_path: &Path, asset_path: &str) -> Result<()> {
+    let content = StaticAssets::get(asset_path)
+        .with_context(|| format!("failed to get embedded file: {}", asset_path))?;
+    let out_path = project_path.join(asset_path);
+    if let Some(parent) = out_path.parent() {
+        create_dir_all(parent).await.with_context(|| {
+            format!(
+                "Failed to create parent directory for: {}",
+                out_path.display()
+            )
+        })?;
     }
 
-    create_dir_all(name).await.with_context(|| format!("failed to create directory: '{}'", name))?;
+    fs::write(&out_path, content.data.as_ref())
+        .await
+        .with_context(|| format!("Failed to write file to disk: {}", out_path.display()))?;
     Ok(())
 }
