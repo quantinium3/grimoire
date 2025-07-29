e814fe1a5ba8deba428fd1ecf02e762b2c8c57f4
quantinium03 <quant@quantinium.dev>
2025-07-30 00:54:07 +0530

    added flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "grimoire";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  };
+}
