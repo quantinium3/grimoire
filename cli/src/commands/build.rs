803560d535e26e4458751c43ac6678272d0ec57c
quantinium03 <quant@quantinium.dev>
2025-07-29 16:50:07 +0530

    build

--- cli/src/commands/build.rs
+++ cli/src/commands/build.rs
@@ -1,8 +1,31 @@
-use crate::error::SSGError;
+use anyhow::Result;
+use which::which;
+use deno_core::{JsRuntime, ModuleCode, ModuleType, ResolutionKind, RuntimeOptions, futures};
+use tokio::process::Command;
 
-pub async fn build_content(include_draft: bool, output_dir: &str) -> Result<(), SSGError> {
+pub async fn build_content(include_draft: bool, output_dir: &str) -> Result<()> {
     println!("building content");
-    println!("include draft: {0}", include_draft);
-    println!("output_dir: {0}", output_dir);
+    let input_dir = "content";
+
+    let output = Command::new(which("grimoire")?)
+        .args(&[
+            "-i",
+            &input_dir,
+            "-o",
+            &output_dir,
+            "--include-drafts",
+            &include_drafts,
+        ])
+        .output()
+        .await?;
+
+    if output.status.success() {
+        println!("Grimoire compiled successfully");
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    } else {
+        println!("Failed to create grimoire.");
+        println!("{}", String::from_utf8_lossy(&output.stderr));
+    }
+
     Ok(())
 }
