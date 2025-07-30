4d8fa2d438544d94e271f360f36aec99f7f27dca
quantinium03 <quant@quantinium.dev>
2025-07-31 01:30:01 +0530

    add init test

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -45,7 +45,7 @@ pub async fn init_project(name: &str) -> Result<()> {
             .with_context(|| format!("Failed to write file: {}", file))?;
     }
 
-    write_examples(content_dir).await?;
+    write_examples(&path.join(content_dir)).await?;
 
     println!("Initialized new project: {}", name);
     println!("{}", "run:".cyan());
@@ -73,30 +73,31 @@ async fn write_embedded_file(project_path: &Path, asset_path: &str) -> Result<()
     Ok(())
 }
 
-async fn write_examples(outdir: &str) -> Result<()> {
+async fn write_examples(outdir: &Path) -> Result<()> {
     let content = get_embedded_file("examples/blog.md")?;
     let now = SystemTime::now();
-    fs::write(
-        format!("{}/blog/{}.md", outdir, print_system_time_to_rfc3339(&now)),
-        content,
-    )
-    .await
-    .with_context(|| format!("Failed to create example in blog dir"))?;
+    let timestamp = print_system_time_to_rfc3339(&now)
+        .replace(":", "-")
+        .replace(".", "-");
+    println!("{}", timestamp);
+
+    let blog_path = Path::new(outdir)
+        .join("blog")
+        .join(format!("{}.md", timestamp));
+    fs::write(&blog_path, &content)
+        .await
+        .with_context(|| format!("Failed to create example in blog dir"))?;
 
     let content = get_embedded_file("examples/static.md")?;
-    fs::write(
-        format!(
-            "{}/static/{}.md",
-            outdir,
-            print_system_time_to_rfc3339(&now)
-        ),
-        content,
-    )
-    .await
-    .with_context(|| format!("Failed to create example in static dir"))?;
+    let static_path = Path::new(outdir)
+        .join("static")
+        .join(format!("{}.md", timestamp));
+    fs::write(static_path, content)
+        .await
+        .with_context(|| format!("Failed to create example in static dir"))?;
 
     let content = get_embedded_file("examples/index.md")?;
-    fs::write(format!("{}/index.md", outdir), content)
+    fs::write(Path::new(outdir).join("index.md"), content)
         .await
         .with_context(|| format!("Failed to create index.md"))?;
 
@@ -109,4 +110,68 @@ fn get_embedded_file(path: &str) -> Result<String, Error> {
 
     String::from_utf8(file.data.to_vec())
         .map_err(|_| anyhow::anyhow!("failed to convert embedded file to string: {}", path))
+}
+
+#[cfg(test)]
+mod tests {
+    use tokio::fs::remove_dir_all;
+    use walkdir::WalkDir;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_init_project() {
+        let name = "test";
+        let path = Path::new(name);
+        let content_dir = "content";
+        init_project(name)
+            .await
+            .map_err(|err| format!("failed to init project {}", err))
+            .unwrap();
+
+        assert!(Path::new(name).exists());
+
+        let dirs = [
+            "templates",
+            &format!("{}/blog", content_dir),
+            &format!("{}/static", content_dir),
+            "static/js",
+            "static/images",
+            "static/styles",
+        ];
+
+        for dir in dirs {
+            let dir_path = path.join(dir);
+            assert!(
+                dir_path.exists(),
+                "Directory {} does not exist",
+                dir_path.display()
+            );
+        }
+
+        let template_files = [
+            ("templates/index.hbs"),
+            ("templates/blog.hbs"),
+            ("templates/page.hbs"),
+        ];
+
+        for file in template_files {
+            let template_path = path.join(file);
+            assert!(
+                template_path.exists(),
+                "template {} does not exist",
+                template_path.display()
+            );
+        }
+
+        for entry in WalkDir::new("test/content") {
+            println!("{:#?}", entry.unwrap().path().display());
+        }
+
+        for dir in dirs {
+            let dir_path = path.join(dir);
+            remove_dir_all(dir_path).await.unwrap();
+        }
+        remove_dir_all(Path::new(name)).await.unwrap();
+    }
 }
