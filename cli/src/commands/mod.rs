8909153bf60600e18a1c871b30e150143c4bf096
quantinium03 <quant@quantinium.dev>
2025-07-31 17:13:14 +0530

    full rewrite to rust

--- cli/src/commands/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod init;
-pub mod add;
-pub mod list;
-pub mod build;
-pub mod serve;
-pub mod clean;
--- flake.nix
+++ flake.nix
@@ -1,116 +1,15 @@
 {
-  description = "Grimoire";
+  description = "A very basic flake";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-  outputs = inputs @ { self, nixpkgs, systems, fenix, ... }:
-    let
-      inherit (nixpkgs) lib;
-      eachSystem = lib.genAttrs (import systems);
-      pkgsFor = eachSystem (system:
-        import nixpkgs {
-          inherit system;
-        });
-      fs = lib.fileset;
-      src = fs.difference
-        (fs.gitTracked ./cli)
-        (fs.unions [
-          (fs.fileFilter (file: file.name == "flake.lock") ./cli)
-          (fs.fileFilter (file: lib.strings.hasInfix ".git" file.name) ./cli)
-          (fs.fileFilter (file: file.hasExt "md") ./cli)
-          (fs.fileFilter (file: file.hasExt "nix") ./cli)
-        ]);
-    in
-    {
-      packages = eachSystem (system:
-        let
-          pkgs = pkgsFor.${system};
 
-          grimoire = pkgs.stdenv.mkDerivation {
-            pname = "grimoire";
-            version = "0.1.0";
-            src = lib.fileset.toSource {
-              root = ./.;
-              fileset = fs.unions [
-                ./build.js
-              ];
-            };
-            nativeBuildInputs = [ pkgs.bun pkgs.file ];
-            buildPhase = ''
-              runHook preBuild
-              if [ ! -f build.js ]; then
-                echo "Error: build.js not found"
-                exit 1
-              fi
-              bun build ./build.js --compile --outfile grimoire
-              if [ ! -x grimoire ]; then
-                echo "Error: grimoire binary not executable"
-                exit 1
-              fi
-              file grimoire
-              ./grimoire || echo "Warning: grimoire execution failed"
-              runHook postBuild
-            '';
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out/bin
-              cp grimoire $out/bin/
-              runHook postInstall
-            '';
+  outputs = { self, nixpkgs }: {
 
-          };
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-          grimoire-cli = pkgs.rustPlatform.buildRustPackage {
-            pname = "grimoire-cli";
-            version = "0.1.0";
-            src = fs.toSource {
-              root = ./cli;
-              fileset = src;
-            };
-            cargoLock = {
-              lockFile = ./cli/Cargo.lock;
-            };
-            buildType = "release";
-            doCheck = false;
-            strictDeps = true;
-            nativeBuildInputs = [ pkgs.installShellFiles ];
-            postInstall = ''
-              mkdir -p $out/bin
-              cp ${grimoire}/bin/grimoire $out/bin/
-            '';
-          };
-        in
-        {
-          default = grimoire-cli;
-          grimoire-cli = grimoire-cli;
-          grimoire = grimoire;
-        });
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-      devShells = eachSystem (system: {
-        default = pkgsFor.${system}.mkShell {
-          packages = with pkgsFor.${system}; [
-            bacon
-            bun
-            bun2nix.packages.${system}.default
-            (fenix.packages.${system}.combine (
-              with fenix.packages.${system}; [
-                stable.cargo
-                stable.clippy
-                stable.rust-analyzer
-                stable.rustc
-                default.rustfmt
-              ]
-            ))
-          ];
-          shellHook = ''
-            export PATH="$PATH:${self.packages.${system}.grimoire}/bin"
-          '';
-        };
-      });
-    };
+  };
 }
