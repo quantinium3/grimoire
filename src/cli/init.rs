57ef91705ec8ffe24ecdab08be17a84bab36f54c
quantinium03 <quant@quantinium.dev>
2025-07-31 18:24:35 +0530

    added init

---
+++ src/cli/init.rs
@@ -0,0 +1,91 @@
+use crate::utils::get_embedded_files;
+use anyhow::{Context, Result};
+use colored::Colorize;
+use rust_embed::RustEmbed;
+use std::{path::Path, time::SystemTime};
+use time_util::print_system_time_to_rfc3339;
+use tokio::fs::{create_dir_all, write};
+
+#[derive(RustEmbed)]
+#[folder = "static"]
+struct StaticAssets;
+
+const GRIMOIRE_CONFIG_NAME: &'static str = "grimoire.config.json";
+
+pub async fn init_project(project_name: &str, content_dir: &str) -> Result<()> {
+    let project_path = Path::new(project_name);
+
+    // initialize dir | templates | examples | config
+    create_init_dirs(project_path, content_dir).await?;
+    create_init_templates(project_path).await?;
+    create_init_examples(project_path, content_dir).await?;
+    create_init_config(project_path).await?;
+
+    println!("Initialized new project: {}", project_name);
+    println!("{}", "run:".bold().cyan());
+    println!("    cd {}", project_name.cyan());
+    println!("    {}", "grimoire build".cyan());
+    Ok(())
+}
+
+async fn create_init_templates(project_path: &Path) -> Result<()> {
+    let files = ["index.html", "page.html", "post.html"];
+    for file in files {
+        let contents = get_embedded_files(file)?;
+        write(project_path.join("templates").join(file), contents)
+            .await
+            .context(format!("Failed to write template: {}", file))?;
+    }
+    Ok(())
+}
+
+async fn create_init_config(project_path: &Path) -> Result<()> {
+    let contents = get_embedded_files("grimoire.config.json")?;
+    write(project_path.join(GRIMOIRE_CONFIG_NAME), contents)
+        .await
+        .context(format!("Failed to write to {}", GRIMOIRE_CONFIG_NAME))?;
+    Ok(())
+}
+
+async fn create_init_examples(project_path: &Path, content_dir: &str) -> Result<()> {
+    let dirs = ["blog", "static"];
+    let now = SystemTime::now();
+    for dir in dirs {
+        let contents = get_embedded_files(&format!("{}.md", dir))?;
+        let timestamp = print_system_time_to_rfc3339(&now);
+        write(
+            project_path
+                .join(content_dir)
+                .join(dir)
+                .join(&format!("{}.md", timestamp)),
+            contents,
+        );
+    }
+
+    // write to /index.md for root
+    let contents = get_embedded_files("index.md")?;
+    write(project_path.join(content_dir).join("index.md"), contents)
+        .await
+        .context("Failed to write to index.md")?;
+    Ok(())
+}
+
+async fn create_init_dirs(project_path: &Path, content_dir: &str) -> Result<()> {
+    let dirs = [
+        "templates",
+        &format!("{}/blog", content_dir),
+        &format!("{}/static", content_dir),
+        "static/js",
+        "static/css",
+        "static/images",
+    ];
+
+    for dir in dirs {
+        let dir_path = project_path.join(dir);
+        create_dir_all(&dir_path)
+            .await
+            .with_context(|| format!("Failed to create directory: {}", dir_path.display()))?;
+    }
+
+    Ok(())
+}
